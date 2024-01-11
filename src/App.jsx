fdea1fc7a4cbb3846c7f436826da866b14985603
Mike Davis <micdavis93@gmail.com>
2024-01-10 17:29:50 -0700

    background image load aborted; fixed hero button css; smaller mobile hero image

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,23 @@
-import { useState, useEffect } from "react"
-import { Routes, Route } from "react-router-dom"
 import "./sass/main.scss"
 
 import Header from "./components/Header"
 import HomePage from "./components/home-page/HomePage"
-import Resume from "./components/Resume"
 import Footer from "./components/Footer"
 
 
 export default function App() {
 
-  
+
 
   return (
     <div className="app">
+
       <Header />
 
-      <Routes>
-        <Route exact path="/" element={<HomePage />} />
-        {/* <Route path="/resume" element={<Resume />} /> */}
-      </Routes>
+      <HomePage />
 
       <Footer />
+
     </div>
   )
 }
