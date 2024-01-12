92d3b9b3e8281cb64375ace1e08b3ff31f4e90dd
Mike Davis <micdavis93@gmail.com>
2024-01-12 12:49:41 -0700

    compressed gifs

--- src/db/projects.js
+++ src/db/projects.js
@@ -4,7 +4,7 @@ export const projects = {
       id: 1,
       title: "Backyard Bowls Food Truck",
       description: "I built this full stack app as my capstone project at Flatiron School, based on my good friend's food truck business (now rebranded to Alpine Eatery). The front end uses React and Tailwind CSS, and the back end is built with Ruby on Rails solely as an API. There is full CRUD",
-      gifName: "byb-project-short.gif",
+      gifName: "byb-project-short-COMP.gif",
       gifID: "1yBJ6orkCnyN8CgKZquANEpwgKYYtkscH",
       htmlID: "backyard-bowls",
       buttons: [
@@ -34,7 +34,7 @@ export const projects = {
       id: 2,
       title: "Daggett Control Company",
       description: "This full stack app was the result of a freelancing contract I took on in the months following my graduation from Flatiron School. I created this site with a React front end using Tailwind CSS, as well as a Ruby on Rails back end API with a custom-built database admin app, built from scratch using Rails Views. This is the most comprehensive website I have built, since it combines so many different techniques and includes a fully deployed front end & back end.",
-      gifName: "dcc-project-short.gif",
+      gifName: "dcc-project-short-COMP.gif",
       gifID: "1OxaIHpjYjscHNOkoOFQ0PnqA5kuSnRzv",
       htmlID: "daggett-control",
       buttons: [
@@ -64,7 +64,7 @@ export const projects = {
       id: 3,
       title: "Vessel Music Services",
       description: "This is a portfolio-type website for a music licensing company. I built this site using my own portfolio site (the one you're currently on) as a template.",
-      gifName: "vms-project-short.gif",
+      gifName: "vms-project-short-COMP.gif",
       gifID: "1uEuFfk0wv5B9nEYyvbvusIdDqUL0TftN",
       htmlID: "vessel-music-services",
       buttons: [
