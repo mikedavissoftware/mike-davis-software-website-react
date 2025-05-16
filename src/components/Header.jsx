fbfc1961277bc7012fb55e374743600f5506a7f4
Michael Davis <micdavis93@gmail.com>
2025-05-16 07:45:10 -0600

    resume 13

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -26,7 +26,7 @@ export default function Header() {
     };
   }, []);
 
-  const resumePath = "./pdf/michael_davis_se_resume_12.pdf"
+  const resumePath = "./pdf/michael_davis_se_resume_13.pdf"
 
   return (
     <header className="header" style={(scrollPosition >= 0) ? ({opacity: `${scrollPosition}%`}) : ({display: "none"})}>
