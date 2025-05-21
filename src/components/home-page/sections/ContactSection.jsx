5590fd673baa350c04a909a62a4b72957899a0bb
Michael Davis <micdavis93@gmail.com>
2025-05-21 17:18:38 -0600

    contact form disappears after success

--- src/components/home-page/sections/ContactSection.jsx
+++ src/components/home-page/sections/ContactSection.jsx
@@ -43,60 +43,63 @@ export default function ContactSection() {
 
         <div className="contact__form-container">
           <form action="#" className="contact__form" onSubmit={handleSubmit}>
-            <div className="contact__form-field">
-              <label className="contact__form-label" htmlFor="name">Name</label>
-              <input
-                required
-                placeholder="Enter Your Name"
-                type="text"
-                className="contact__form-input"
-                name="name"
-                id="name"
-                value={formData.name}
-                onChange={handleChange}
-              />
+            <div style={{display: `${(success) ? ("none") : ("")}`}}>
+              <div className="contact__form-field">
+                <label className="contact__form-label" htmlFor="name">Name</label>
+                <input
+                  required
+                  placeholder="Enter Your Name"
+                  type="text"
+                  className="contact__form-input"
+                  name="name"
+                  id="name"
+                  value={formData.name}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="contact__form-field">
+                <label className="contact__form-label" htmlFor="email">Email</label>
+                <input
+                  required
+                  placeholder="Enter Your Email"
+                  type="text"
+                  className="contact__form-input"
+                  name="email"
+                  id="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="contact__form-field">
+                <label className="contact__form-label" htmlFor="message">Message</label>
+                <textarea
+                  required
+                  cols="30"
+                  rows="10"
+                  className="contact__form-input"
+                  placeholder="Enter Your Message"
+                  name="message"
+                  id="message"
+                  value={formData.message}
+                  onChange={handleChange}
+                ></textarea>
+              </div>
+
+              <button type="submit" className="btn btn--theme contact__btn">
+                Submit
+              </button>
+
             </div>
-            <div className="contact__form-field">
-              <label className="contact__form-label" htmlFor="email">Email</label>
-              <input
-                required
-                placeholder="Enter Your Email"
-                type="text"
-                className="contact__form-input"
-                name="email"
-                id="email"
-                value={formData.email}
-                onChange={handleChange}
-              />
-            </div>
-            <div className="contact__form-field">
-              <label className="contact__form-label" htmlFor="message">Message</label>
-              <textarea
-                required
-                cols="30"
-                rows="10"
-                className="contact__form-input"
-                placeholder="Enter Your Message"
-                name="message"
-                id="message"
-                value={formData.message}
-                onChange={handleChange}
-              ></textarea>
-            </div>
-            
-            {(success != "") ? (
+          </form>
+
+          {(success != "") ? (
               (success) ? (
                 <p className="contact__form-status success">Contact form submitted successfully! I will be in touch as soon as I can!</p>
               ) : (
                 <p className="contact__form-status error">There was an error submitting the contact form. Please check your entry and try again, or reach out on <a href="https://www.linkedin.com/in/mikedavissoftware">LinkedIn</a>.</p>
               )
             ) : (null)
-            }
-
-            <button type="submit" className="btn btn--theme contact__btn">
-              Submit
-            </button>
-          </form>
+          }
         </div>
         {}
       </div>
